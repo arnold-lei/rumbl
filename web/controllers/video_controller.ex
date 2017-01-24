99a8a81d276d55013bbc5c39634e27de35cdb9b9
Arnold Lei <arnold.lei88@gmail.com>
2017-01-24 18:28:42 -0500

    adding categories

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -2,6 +2,9 @@ defmodule Rumbl.VideoController do
   use Rumbl.Web, :controller
 
   alias Rumbl.Video
+  alias Rumbl.Category
+
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   def index(conn, _params, user) do
     videos = Repo.all(user_videos(user))
@@ -72,6 +75,16 @@ defmodule Rumbl.VideoController do
   defp user_videos(user) do
       assoc(user, :videos)
   end
+
+  defp load_categories(conn, _) do
+      query =
+          Category
+          |> Category.alphabetical
+          |> Category.names_and_ids
+         categories = Repo.all query
+         assign(conn, :categories, categories)
+  end
+
   def action(conn, _) do
       apply(__MODULE__, action_name(conn),
         [conn, conn.params, conn.assigns.current_user])
