952bffb566383e353004cc1f15e8d8530aca4dd3
Arnold Lei <arnold.lei88@gmail.com>
2017-01-27 14:43:05 -0500

    Added error validation for categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -17,10 +17,10 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description, :category_id])
-    |> validate_required([:url, :title, :description])
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> assoc_constraint(:category)
 
   end
 end
