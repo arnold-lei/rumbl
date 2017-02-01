c381f8c10b34f5753a07b75d8fc0769ba745829e
Arnold Lei <arnold.lei88@gmail.com>
2017-02-01 18:47:26 -0500

    added tests to ensure users can't edit and view other users videos

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -25,7 +25,7 @@ defmodule Rumbl.VideoControllerTest do
             assert conn.halted
         end)
     end
-    
+
     @tag login_as: "max"
     test "List all user's videos on index", %{conn: conn, user: user} do
         user_video = insert_video(user, title: "funny cats")
@@ -36,5 +36,47 @@ defmodule Rumbl.VideoControllerTest do
         assert String.contains?(conn.resp_body, user_video.title)
         refute String.contains?(conn.resp_body, other_video.title)
 
+    end
+
+    alias Rumbl.Video
+    @valid_attrs %{url: "http://youtu.be", title: "vid", description: "a vid"}
+    @invalid_attrs %{title: "invalid"}
+
+    defp video_count(query), do: Repo.one(from v in query, select: count(v.id))
+    @tag login_as: "max"
+    test "creates user video and redirects", %{conn: conn, user: user} do
+        conn = post conn, video_path(conn, :create), video: @valid_attrs
+        assert redirected_to(conn) == video_path(conn, :index)
+        assert Repo.get_by!(Video, @valid_attrs).user_id == user.id
+    end
+
+    @tag login_as: "max"
+    test "does not create video and renders errors when invalid", %{conn: conn} do
+        count_before = video_count(Video)
+        conn = post conn, video_path(conn, :create), video: @invalid_attrs
+        assert html_response(conn, 200) =~ "check the errors"
+        assert video_count(Video) == count_before
+    end
+
+    @tag login_as: "max"
+    test "authorize actions against access by other users",
+        %{user: owner, conn: conn} do
+
+        video = insert_video(owner, @valid_attrs)
+        non_owner = insert_user(username: "sneaky")
+        conn = assign(conn, :current_user, non_owner)
+
+        assert_error_sent :not_found, fn ->
+            get(conn, video_path(conn, :show, non_owner))
+        end
+        assert_error_sent :not_found, fn ->
+            get(conn, video_path(conn, :edit, video))
+        end
+        assert_error_sent :not_found, fn ->
+            put(conn, video_path(conn, :update, video, video: @valid_attrs))
+        end
+        assert_error_sent :not_found, fn ->
+            delete(conn, video_path(conn, :delete, video))
+        end
     end
 end
