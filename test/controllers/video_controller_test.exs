b96e700e5040510de8d3241fcfb043f7d45346fa
Arnold Lei <arnold.lei88@gmail.com>
2017-02-01 15:36:17 -0500

    fixed the test bug

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -11,18 +11,6 @@ defmodule Rumbl.VideoControllerTest do
         end
     end
 
-    @tag login_as: "max"
-    test "List all user's videos on index", %{conn: conn, user: user} do
-        user_video = insert_video(user, title: "funny cats")
-        other_video = insert_video(insert_user(username: "other"), title: "another video")
-
-        conn = get conn, video_path(conn, :index)
-        assert html_response(conn, 200) =~ ~r/Listing videos/
-        assert String.contains?(conn.resp_body, user_video.title)
-        refute String.contains?(conn.resp_body, other_video.title)
-
-    end
-
     test "requires user authentication on all actions", %{conn: conn} do
         Enum.each([
             get(build_conn, video_path(build_conn, :new)),
@@ -36,5 +24,17 @@ defmodule Rumbl.VideoControllerTest do
             assert html_response(conn, 302)
             assert conn.halted
         end)
+    end
+    
+    @tag login_as: "max"
+    test "List all user's videos on index", %{conn: conn, user: user} do
+        user_video = insert_video(user, title: "funny cats")
+        other_video = insert_video(insert_user(username: "other"), title: "another video")
+
+        conn = get conn, video_path(conn, :index)
+        assert html_response(conn, 200) =~ ~r/Listing videos/
+        assert String.contains?(conn.resp_body, user_video.title)
+        refute String.contains?(conn.resp_body, other_video.title)
+
     end
 end
