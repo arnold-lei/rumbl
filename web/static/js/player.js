20f39a0588d68102835040e2c3470bbafba02cae
Arnold Lei <arnold.lei88@gmail.com>
2017-02-14 00:52:31 -0500

    working on video player

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -24,4 +24,4 @@ let Player = {
     getCurrentTime(){return Math.floor(this.player.getCurrentTime() * 1000)},
     seekTo(millsec){ return this.player.seekTo(millsec / 1000)}
 }
- export default Player
+export default Player
