bb392b8792ab8ee5bfc854552e6dffcba836719d
Arnold Lei <arnold.lei88@gmail.com>
2017-02-15 18:17:20 -0500

    video player is now working

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -12,7 +12,9 @@
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
+
 import Player from "./player"
+let video = document.getElementById("video")
 
 if(video) {
     Player.init(video.id, video.getAttribute("data-player-id"), () => {
