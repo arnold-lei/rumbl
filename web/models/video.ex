934d383aa3cc6e1d86b37bfc2c297a77de609f81
Arnold <arnold.lei88@gmail.com>
2017-02-18 23:24:15 -0500

    working on video slug model

--- web/models/video.ex
+++ web/models/video.ex
@@ -21,7 +21,14 @@ defmodule Rumbl.Video do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
-
+  end
+  defp slugify_title(changeset) do
+      if title = get_change(changeset, :title) do
+          put_change(changeset, :slug, slugify(title))
+      else
+          changeset
+      end
   end
 end
